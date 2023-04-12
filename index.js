38eaa05e399f456f13be8b5819ca2a8c23c0a8e5
Cristian <cristianvasquez@gmail.com>
2023-04-12 10:44:53 +0200

    feat: update sparql-http-client

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// eslint-disable-next-line node/no-deprecated-api
-import { resolve } from 'url'
 import debugLib from 'debug'
-import nodeFetch from 'node-fetch'
-import SparqlHttpClient from 'sparql-http-client'
+import ParsingClient from 'sparql-http-client/ParsingClient.js'
+import SimpleClient from 'sparql-http-client/SimpleClient.js'
 
 const debug = debugLib('trifid:handler-sparql')
-SparqlHttpClient.fetch = nodeFetch
 
 const defaults = {
   authentication: false,
@@ -28,7 +25,8 @@ export class SparqlHandler {
     this.resourceGraphQuery = options.resourceGraphQuery
     this.containerExistsQuery = options.containerExistsQuery
     this.containerGraphQuery = options.containerGraphQuery
-    this.client = new SparqlHttpClient({ endpointUrl: options.endpointUrl })
+    this.parsingClient = new ParsingClient({ endpointUrl: options.endpointUrl })
+    this.simpleClient = new SimpleClient({ endpointUrl: options.endpointUrl })
   }
 
   buildQueryOptions () {
@@ -62,29 +60,22 @@ export class SparqlHandler {
   async exists (iri, query) {
     debug('SPARQL exists query for IRI <' + iri + '> : ' + query)
 
-    const res = await this.client.selectQuery(query, this.buildQueryOptions())
-    const status = res.status
-    if (status !== 200) {
-      return { status, undefined }
+    try {
+      const exists = await this.parsingClient.query.ask(query,
+        this.buildQueryOptions())
+      return { exists, status: 200 }
+    } catch (error) {
+      return { status: error.status }
     }
-    const json = await res.json()
-    const exists = json.boolean
-    return { status, exists }
   }
 
   async graphStream (iri, query, accept) {
     debug('SPARQL query for IRI <' + iri + '> : ' + query)
 
-    const queryOptions = this.buildQueryOptions()
+    const headers = this.buildQueryOptions()
+    headers.accept = accept
 
-    queryOptions.accept = accept
-
-    const res = await this.client.constructQuery(query, queryOptions)
-    if (res.status !== 200) {
-      return { status: res.status }
-    }
-    const headers = {}
-
+    const res = await this.simpleClient.query.construct(query, { headers })
     res.headers.forEach((value, name) => {
       // stream will be decoded by the client -> remove content-encoding header
       if (name === 'content-encoding') {
@@ -100,11 +91,34 @@ export class SparqlHandler {
   }
 
   handle (req, res, next) {
-    if (req.method === 'GET') {
-      this.get(req, res, next, req.iri)
-    } else {
-      next()
+    switch (req.method) {
+      case 'HEAD':
+        return this.head(req, res, next, req.iri)
+      case 'GET':
+        return this.get(req, res, next, req.iri)
     }
+
+    return next()
+  }
+
+  async head (_req, res, next, iri) {
+    iri = encodeURI(iri)
+
+    debug('handle HEAD request for IRI <' + iri + '>')
+
+    const isContainer = this.resourceNoSlash && iri.endsWith('/')
+    const queryExist = isContainer ? this.buildContainerExistsQuery(iri) : this.buildResourceExistsQuery(iri)
+
+    const { status, exists } = await this.exists(iri, queryExist)
+
+    if (status !== 200) {
+      res.sendStatus(status)
+      return next()
+    } else if (!exists) {
+      return next()
+    }
+
+    res.sendStatus(status)
   }
 
   async get (req, res, next, iri) {
@@ -146,10 +160,12 @@ export const factory = trifid => {
   const endpoint = endpointUrl || '/query'
 
   return (req, res, next) => {
+    const absoluteUrl = res.locals.camouflageRewriteOriginalUrl || req.absoluteUrl()
+    const endpointUrl = new URL(endpoint, absoluteUrl)
     const handler = new SparqlHandler({
       ...defaults,
       ...config,
-      endpointUrl: resolve(req.absoluteUrl(), endpoint)
+      endpointUrl: endpointUrl.toString()
     })
     handler.handle(req, res, next)
   }
